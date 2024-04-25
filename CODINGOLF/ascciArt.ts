a0265d14ad7d09e4075997c668524863ccdc7bf3
caloivan <caloivan@gmail.com>
2024-04-24 18:32:53 -0700

    Update ascciArt.ts

--- CODINGOLF/ascciArt.ts
+++ CODINGOLF/ascciArt.ts
@@ -1,2 +1,2 @@
-let w=Number(readline()),h=Number(readline()),t=readline().toUpperCase(),i,o,b,c;
-for(i=0;i<h;i++){const row=readline();o="";for(let l of t){b=l.charCodeAt(0)-'A'.charCodeAt(0);b=b<0?26:b>25?26:b;c=b*w;o+=row.substring(c,c+w);}console.log(o)}
+let w=Number(readline()),h=readline(),t=readline().toUpperCase(),i,o,b,c,r
+for(i=0;i<h;i++){r=readline();o="";for(let l of t){b=l.charCodeAt(0)-'A'.charCodeAt(0);b=b<0||b>25?26:b;c=b*w;o+=r.substring(c,c+w)}console.log(o)}
